4e93bee32dc4aa512ec07d1b941aec6ebfae02b6
Licia <licia.o.bailey@gmail.com>
2021-02-09 17:03:09 -0700

    implicit and explicit examples

--- phase-1-assignments/type-casting/src/explicit/ExplicitTypeCasting.java
+++ phase-1-assignments/type-casting/src/explicit/ExplicitTypeCasting.java
@@ -1,5 +1,22 @@
 package explicit;
 
 public class ExplicitTypeCasting {
-
+	public static void main(String[] args) {
+		System.out.println("Explicit Casting Example");
+		double de=499.77777;
+		System.out.println("double = "+de);
+		int i=(int)de;
+		System.out.println("integer = "+i);
+		float fe=99.66f;
+		System.out.println("float = "+fe);
+		long le=(long)fe;
+		System.out.println("long = "+le);
+		
+		char c='L';
+		int ci=c;
+		ci=77;
+		c=(char) ci;
+		System.out.println("char = "+c);
+		System.out.println("char decimal = "+ci);
+	}
 }
--- phase-1-assignments/type-casting/src/implicit/ImplicitTypeCasting.java
+++ phase-1-assignments/type-casting/src/implicit/ImplicitTypeCasting.java
@@ -6,19 +6,19 @@ public class ImplicitTypeCasting {
 		int x=500;
 		System.out.println("integer = "+x);
 		double d=x; 
-		System.out.println("double = "+d);
-				
+		System.out.println("double = "+d);	
 		long l=x; 
-		System.out.println("long = "+l);
-				
+		System.out.println("long = "+l);		
 		float f=l; 
 		System.out.println("float = "+f);
 		f=499.77777f;
 		d=f; 
 		System.out.println("decimal = "+d);
 		
-		char c='Z';
-		System.out.println("c = "+c);
+		char c='L';
+		int ci=c;
+		System.out.println("char = "+c);
+		System.out.println("char dec = "+ci);
 	}
 
 }
