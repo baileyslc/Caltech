dc823b2465f3686497063a6bfdbfc7629d40a837
Licia <licia.o.bailey@gmail.com>
2021-02-21 23:53:50 -0700

    welcome screen

---
+++ phase-1-lockedme-final-project/src/pk1/Main.java
@@ -0,0 +1,10 @@
+package pk1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to LockMe | The safe and secure way to manage your files | Created by Licia Bailey");
+		System.out.println("Options/features to accept user input");
+	}
+
+}
